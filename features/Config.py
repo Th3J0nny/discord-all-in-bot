cd62300ff621ac75405c2604c61e9c7d4e645303
Jonathan Busch <jonathanbusch97@gmx.de>
2022-01-13 20:25:15 +0100

    Added Config feature which currently allows to change the prefix of the bot for every individual server

---
+++ features/Config.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+
+from Utility import Storage
+from Utility.Utils import *
+
+steps = dict()
+
+
+def is_owner(message):
+    return message.author.id == message.guild.owner_id
+
+
+class Setup(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='change-prefix', aliases=['changeprefix'])
+    async def change_prefix(self, ctx, arg1):
+        if is_owner(ctx):
+            if arg1:
+                Storage.set_prefix(ctx.guild.id, arg1)
+                self.bot.command_prefix = arg1
+                await send(ctx, description="Command prefix successfully changed to {}.".format(arg1))
+            else:
+                await send_error(ctx, description="Alias cannot be empty.")
+        else:
+            await send_error(ctx, description="Insufficient permissions. Please ask the server owner")
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Setup(bot))

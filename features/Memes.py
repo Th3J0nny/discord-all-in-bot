4e5612aa9fa1cdb2d6078ddbc20024133faa0df8
Jonathan Busch <jonathanbusch97@gmx.de>
2022-01-13 21:11:34 +0100

    Only allow SFW images from the meme command

--- features/Memes.py
+++ features/Memes.py
@@ -19,15 +19,24 @@ class Memes(commands.Cog):
         r = requests.get("https://www.reddit.com/r/{}.json?sort=top&t=year".format(arg1),
                          headers={'User-agent': 'All-In-Discord-Bot'})
         if r.status_code == 200:
+            was_nsfw = False
             random.seed(time.time())
             for x in range(25):
                 j = json.loads(r.text)
                 index = random.randint(0, len(j['data']['children']) - 1)
-                img_url = j['data']['children'][index]['data']['url']
+                data = j['data']['children'][index]['data']
+                if data['over_18']:
+                    was_nsfw = True
+                    continue
+                img_url = data['url']
                 if img_url and ('.jpg' in img_url or '.png' in img_url or '.jpeg' in img_url):
                     await send(ctx.channel, title='Random image from subreddit \'{}\''.format(arg1), image=img_url)
                     return
-            await send(ctx.channel, description='No random image found on subreddit \'{}\''.format(arg1))
+            if was_nsfw:
+                await send(ctx.channel,
+                           description='No random image found on subreddit \'{}\' that was SFW'.format(arg1))
+            else:
+                await send(ctx.channel, description='No random image found on subreddit \'{}\''.format(arg1))
         else:
             await send(ctx.channel, description='Subreddit not found or connection error')
 

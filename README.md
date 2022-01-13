3f36c89f1ec7d666019170651b4aeaa3af0e5668
Jonathan Busch <jonathanbusch97@gmx.de>
2022-01-13 19:37:03 +0100

    Added command line argument to exclude features on start up. Added this to README.md

--- README.md
+++ README.md
@@ -5,13 +5,22 @@ A simple and modular Discord bot with various functionalities.
 ## How to use the bot?
 
 Simple! Just invite the bot to your server using
-this [link](https://discord.com/api/oauth2/authorize?client_id=803637393143889980&permissions=274878154752&scope=bot)
+this [link,](https://discord.com/api/oauth2/authorize?client_id=803637393143889980&permissions=274878154752&scope=bot)
 and you are good to go!
 
 ## Development
 
 I'm doing this project for fun. The bot is developed in a way, that each feature has their own module. New models can be
 added by everyone. I'm happy about all contributions to this project. Especially about **feature ideas**!
+
+### Arguments
+
+```
+optional arguments:
+  -h, --help            show this help message and exit
+  -e [EXCLUDE_FEATURES ...], --exclude-features [EXCLUDE_FEATURES ...]
+                        Use this to exclude one or more features when starting the bot.
+```
 
 ### Adding a feature
 
@@ -54,4 +63,4 @@ Additionally you'll need to create a Discord developer account: https://discord.
 ## TODO
 
 - Setup logging
-- Provide CLI options, e.g., to exclude features
+- Provide CLI options, e.g., to exclude features (DONE) and ...?
--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from discord.ext import commands
@@ -16,18 +17,28 @@ def load_features():
         name = f.split('.')[0]
         if name.startswith('__') or name == 'Template':
             continue
+        if name.lower() in excluded_features:
+            print('\t' + name + ' X')
+            continue
         bot.load_extension('features.' + name)
         print('\t' + name + ' ✓')
 
 
 if __name__ == '__main__':
-    print("Welcome to the one and only")
+    print("\nWelcome to the one and only")
     print("           _ _            _____                  ____        _   ")
     print("     /\   | | |          |_   _|                |  _ \      | |  ")
     print("    /  \  | | |  ______    | |  _ __    ______  | |_) | ___ | |_ ")
     print("   / /\ \ | | | |______|   | | | '_ \  |______| |  _ < / _ \| __|")
     print("  / ____ \| | |           _| |_| | | |          | |_) | (_) | |_ ")
-    print(" /_/    \_\_|_|          |_____|_| |_|          |____/ \___/ \__|")
+    print(" /_/    \_\_|_|          |_____|_| |_|          |____/ \___/ \__|\n")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--exclude-features', nargs='*', default=[], help="Use this to exclude one or more "
+                                                                                "features when starting the bot.")
+
+    args = vars(parser.parse_args())
+    excluded_features = [name.lower() for name in args['exclude_features']]
 
     bot = commands.Bot(command_prefix=Storage.get_prefix)
     load_features()

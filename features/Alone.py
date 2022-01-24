60180c93c125ff5475b264eaf4dc9bf9f4d00a1e
muderjul <mudersju@student.hu-berlin.de>
2022-01-24 23:51:47 +0100

    added missing cases, implemented default channel

--- features/Alone.py
+++ features/Alone.py
@@ -11,6 +11,15 @@ steps = dict()
 def is_owner(message):
     return message.author.id == message.guild.owner_id
 
+# defines the text to send on loneliness detection
+async def send_static(member, duration):
+    channel = Storage.read(member.guild.id, "default_channel")
+    if channel is None:
+        channel = member.guild.system_channel
+
+    await send(channel, description="{} spent {} seconds alone in a voice channel.".format(member.display_name,
+                                                                                          duration))
+
 
 # A class that tracks how long someone spends alone in a voice channel
 class Alone(commands.Cog):
@@ -24,53 +33,49 @@ class Alone(commands.Cog):
         # read config and skip if feature is disabled
         alone_enabled = bool(Storage.read(member.guild.id, "alone_enabled"))
         if alone_enabled is None or not bool(alone_enabled):
+            # feature not enabled, early exit
             return
 
         # check nature of state update
-        if before.channel is None and after.channel is not None:
-            # joined channel
-            if len(after.channel.members) == 1:
-                # alone
-                self.users_alone[member.id] = monotonic()
-            elif len(after.channel.members) == 2:
-                # somebody joined channel where somebody was alone before
-                inters = set(after.channel.members).intersect(set(self.users_alone.keys()))
-                # check if we 
-                if len(inters) > 1:
-                    # unambiguous result
-                    for member_id in inters:
-                        del self.users_alone[member_id]
-                elif len(inters) == 1:
-                    # we have an exact record
-                    lonely_person = inters.pop()
-                    duration = monotonic() - self.users_alone.pop(lonely_person)
+        if after.channel is not None:
+            self.check_target_channel(after.channel, member)
 
-                    channel = member.guild.system_channel
-                    if channel is None:
-                        # channel not available
-                        return
+        if before.channel is not None:
+            self.check_source_channel(before.channel, member)
 
-                    await send(channel, description="User {} spent {} alone in a voice channel".format(
-                        member.guild.get_member(lonely_person).display_name, duration))
+    async def check_source_channel(self, channel, member):
+        # left channel
+        if len(channel.members) == 1:
+            # member left, so somebody is alone now
+            self.users_alone[channel.members[0].id] = monotonic()
+        elif len(channel.members) == 0:
+            # member was alone until now
+            if member.id in set(self.users_alone):
+                duration = monotonic() - self.users_alone.pop(member.id)
 
-        elif before.channel is not None and after.channel is None:
-            # left channel
-            if len(before.channel.members) == 1:
-                # member left, so somebody is alone now
-                self.users_alone[before.channel.members[0].id] = monotonic()
-            elif len(before.channel.members) == 0:
-                # member was alone until now
-                if member.id in set(self.users_alone):
-                    duration = monotonic() - self.users_alone.pop(member.id)
+                await send_static(member, duration)
 
-                    channel = member.guild.system_channel
-                    if channel is None:
-                        # channel not available
-                        return
+    async def check_target_channel(self, channel, member):
+        # joined channel
+        if len(channel.members) == 1:
+            # joined an empty channel
+            self.users_alone[member.id] = monotonic()
+        elif len(channel.members) == 2:
+            # somebody joined channel where somebody was alone before
+            inters = set(after.channel.members).intersection(set(self.users_alone.keys()))
+            # check if we 
+            if len(inters) > 1:
+                # unambiguous result
+                for member_id in inters:
+                    del self.users_alone[member_id]
+            elif len(inters) == 1:
+                # we have an exact record
+                lonely_person = inters.pop()
+                duration = monotonic() - self.users_alone.pop(lonely_person)
 
-                    await send(channel,
-                               description="User {} spent {} alone in a voice channel".format(member.display_name,
-                                                                                              duration))
+                await send_static(member, duration)
+
+
 
     @commands.command(name='change-alone', aliases=['changealone'])
     async def change_alone(self, ctx, arg1):
@@ -78,7 +83,7 @@ class Alone(commands.Cog):
             Storage.write(ctx.guild.id, "alone_enabled", arg1)
             await send(ctx, description="Command Alone status successfully set to {}.".format(bool(arg1)))
         else:
-            await send_error(ctx, description="Insufficient permissions. Please ask the server owner")
+            await send_error(ctx, description="Insufficient permissions. Please ask the server owner.")
 
 
 # Setup function needed for registration of the feature Cog
--- features/Config.py
+++ features/Config.py
@@ -25,7 +25,19 @@ class Setup(commands.Cog):
             else:
                 await send_error(ctx, description="Alias cannot be empty.")
         else:
-            await send_error(ctx, description="Insufficient permissions. Please ask the server owner")
+            await send_error(ctx, description="Insufficient permissions. Please ask the server owner.")
+
+
+    @commands.command(name='default-channel', aliases=['defaultchannel'])
+    async def change_alone(self, ctx, arg1):
+        if is_owner(ctx):
+            if arg1 in await ctx.guild.fetch_channels():
+                Storage.write(ctx.guild.id, "default_channel", arg1)
+                await send(ctx, description="Default channel successfully set to {}.".format(arg1))
+            else:
+                await send_error(ctx, description="Target default channel does not exist.")
+        else:
+            await send_error(ctx, description="Insufficient permissions. Please ask the server owner.")
 
 
 def setup(bot: commands.Bot):
